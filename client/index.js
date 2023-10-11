920f0ccc57766812724ee09b76c0db365265ce0b
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2023-10-11 17:13:51 +0530

    search list page done, except the add to favourite button

--- client/index.js
+++ client/index.js
@@ -20,6 +20,29 @@ async function updateResponse(){
     
       console.log(list)
 
-      
+      const listDiv = document.getElementById('result-list')
+
+      for(var i=0 ; i<list.length ; i++){
+        const mainBox = document.createElement('div')
+        const box = document.createElement('div')
+        const title  = document.createElement('h4')
+        const year = document.createElement('h5')
+        const image = document.createElement('img')
+        const sideBox = document.createElement('div')
+        title.innerText = list[i].Title
+        year.innerText = list[i].Year
+        image.src = list[i].Poster
+        image.width = 100
+        image.height = 150
+        sideBox.appendChild(title)
+        sideBox.appendChild(year)
+        box.appendChild(image)
+        mainBox.appendChild(box)
+        mainBox.appendChild(sideBox)
+        mainBox.classList.add('inner-box-list')
+        sideBox.classList.add('side-box-list')
+
+        listDiv.appendChild(mainBox)
+      }
       
 }
